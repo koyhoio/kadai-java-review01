3ea529c6b71b483686f4c04111731e0c2e5c83cb
koyhoio <f_nyu27@yahoo.co.jp>
2024-05-20 23:17:16 +0900

    ,変数名変更

--- src/Review01.java
+++ src/Review01.java
@@ -3,13 +3,13 @@ public class Review01 {
 
     public static void main(String[] args) {
         int result = tax(1500,10);
-        int p = 1500;
-        int taxin = p+result;
-        System.out.println(p+"円の商品の税込価格は"+taxin+"円（消費税は"+result+"円）です。");
+        int price = 1500;
+        int taxin = price+result;
+        System.out.println(price+"円の商品の税込価格は"+taxin+"円（消費税は"+result+"円）です。");
 
     }
-    public static int tax(int price, int taxp) {
-        int result = price/taxp;
+    public static int tax(int p, int taxp) {
+        int result = p/taxp;
 
         return result;
 
