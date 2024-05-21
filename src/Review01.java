2a077160c905b3bc3599a5fbdc9c17fd882492ee
koyhoio <f_nyu27@yahoo.co.jp>
2024-05-21 23:07:09 +0900

    double/int型の変更

--- src/Review01.java
+++ src/Review01.java
@@ -2,16 +2,18 @@
 public class Review01 {
 
     public static void main(String[] args) {
-        int result = tax(1500,10);
+        int i = tax(1500,5);
         int price = 1500;
-        int taxin = price+result;
-        System.out.println(price+"円の商品の税込価格は"+taxin+"円（消費税は"+result+"円）です。");
+        int taxin = price+i;
+        System.out.println(price+"円の商品の税込価格は"+taxin+"円（消費税は"+i+"円）です。");
 
     }
     public static int tax(int p, int taxp) {
-        int result = p/taxp;
+        double tanp = (double)taxp/100;
+        double result = p*tanp;
+        int i = (int)result;
 
-        return result;
+        return i;
 
     }
 
